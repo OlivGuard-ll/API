c2eede5f4eaa719f345c3e7754ccc84bf26983bb
HenryGabrielGarcia <henry.garcia@sptech.school>
2025-05-20 22:08:53 -0300

    Ajustes - Adicionei a API ao projeto

---
+++ Com API/src/models/empresaModel.js
@@ -0,0 +1,24 @@
+var database = require("../database/config");
+
+function buscarPorId(id) {
+  var instrucaoSql = `SELECT * FROM empresa WHERE id = '${id}'`;
+
+  return database.executar(instrucaoSql);
+}
+
+function listar() {
+  var instrucaoSql = `SELECT idEmpresa, razaoSocial, cnpj, codigo_ativacao FROM empresa`;
+  return database.executar(instrucaoSql);
+}
+
+function buscarPorCnpj(cnpj) {
+  var instrucaoSql = `SELECT * FROM empresa WHERE cnpj = '${cnpj}'`;
+  return database.executar(instrucaoSql);
+}
+
+function cadastrar(razaoSocial, cnpj) {
+  var instrucaoSql = `INSERT INTO empresa (razaoSocial, cnpj) VALUES ('${razaoSocial}', '${cnpj}')`;
+  return database.executar(instrucaoSql);
+}
+
+module.exports = { buscarPorCnpj, buscarPorId, cadastrar, listar };
---
+++ Com API/src/models/usuarioModel.js
@@ -0,0 +1,27 @@
+var database = require("../database/config")
+
+function autenticar(email, senha) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
+    var instrucaoSql = `
+        SELECT idUsuario, nome, sobrenome ,email, senha, fkEmpresa as empresaId FROM usuario WHERE email = '${email}' AND senha = '${senha}';
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+
+function cadastrar(nome, sobrenome, email, senha, fkEmpresa) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, sobrenome ,email, senha, fkEmpresa);
+    var instrucaoSql = `
+        INSERT INTO usuario (nome, sobrenome, email, senha, fkEmpresa) VALUES ('${nome}', '${sobrenome}', '${email}', '${senha}', '${fkEmpresa}');
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+    
+}
+
+
+module.exports = {
+    autenticar,
+    cadastrar
+};
