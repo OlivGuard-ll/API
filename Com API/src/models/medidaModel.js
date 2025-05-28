6e69ca8cc68b3021525039d46747de5089234bb2
Felipe-Macedo-Sptech <felipe.correia@sptech.school>
2025-05-27 21:48:07 -0300

    Dashboard atualizando automaticamente

--- Com API/src/models/medidaModel.js
+++ Com API/src/models/medidaModel.js
@@ -18,12 +18,11 @@ function buscarUltimasMedidas(idAquario, limite_linhas) {
 function buscarMedidasEmTempoReal(idAquario) {
 
     var instrucaoSql = `SELECT 
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,
-                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico, 
-                        fk_aquario 
-                        FROM medida WHERE fk_aquario = ${idAquario} 
-                    ORDER BY id DESC LIMIT 1`;
+        leitura as leitura,
+		DATE_FORMAT(dtLeitura,'%H:%i:%s') as dtLeitura_grafico, 
+		fkSensor 
+		FROM Leitura WHERE fkSensor = ${idAquario}
+                    ORDER BY idLeitura DESC LIMIT 1;`;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
